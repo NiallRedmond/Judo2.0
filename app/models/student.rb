568dd87b9e68f3a758351f72d16ea0b439b0a0b1
Niall <niall.redmond@hotmail.com>
2016-11-04 18:09:40 +0000

    fixed views to allow for booleans+more bootsrap

--- app/models/student.rb
+++ app/models/student.rb
@@ -9,12 +9,24 @@ class Student < ActiveRecord::Base
 	validates_uniqueness_of :email
 	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/}
 	validates :dob, presence: true
+	validates :password ,:length => {:within => 4..40}
 	
 	has_secure_password
+	
 	
 	def age
 		d = Date.new(Date.today.year, dob.month, dob.day)
 		d.year - dob.year - (d > Date.today ? 1 : 0)
+	end
+	
+	def gen
+		if @student.gender == false 
+			gen = 'male'
+			gen 
+		else 
+			gen = 'female'
+			gen 
+		end
 	end
 	
 end
